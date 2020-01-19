7146d7f830d58aead3be7b3d6dfd159ca71836e9
Inon Bercu <inonbr@gmail.com>
2020-01-19 11:39:16 +0200

    fixed throw new TypeError( + bug

--- models/User.js
+++ models/User.js
@@ -2,19 +2,23 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: {
+    required: true,
     type: String
   },
   email: {
+    required: true,
     type: String,
     unique: true
   },
   password: {
+    required: true,
     type: String
   },
   avatar: {
     type: String
   },
   date: {
+    type: Date,
     default: Date.now
   }
 });
