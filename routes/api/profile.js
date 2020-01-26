76685be8a2b26cb3b8851c4c1cfaa5a85ea93187
Inon Bercu <inonbr@gmail.com>
2020-01-26 12:15:15 +0200

    add user exp

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -166,11 +166,72 @@ router.delete('/', auth, async (req, res) => {
     // remove user
     await User.findOneAndRemove({ _id: req.user.id });
 
-    res.json({ msg: 'user removed' });
+    res.json({ msg: 'User removed' });
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
 });
+
+// @route:  PUT api/profile/experience
+// @desc:   Add profile experience
+// @access: Privet
+
+router.put(
+  '/experience',
+  [
+    auth,
+    [
+      check('title', 'Title is is required')
+        .not()
+        .isEmpty(),
+      check('company', 'Company is is required')
+        .not()
+        .isEmpty(),
+      check('from', 'From date is is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    } = req.body;
+
+    const newExp = {
+      title,
+      company,
+      location,
+      from,
+      to,
+      current,
+      description
+    };
+
+    try {
+      const profile = await Profile.findOne({ user: req.user.id });
+
+      profile.experience.unshift(newExp);
+
+      await profile.save();
+
+      res.json(profile);
+    } catch (error) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
