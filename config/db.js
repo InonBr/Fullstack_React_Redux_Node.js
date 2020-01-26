c0e2cee4ff346ae22f216ec14079bb4c2bf0f3a8
Inon Bercu <inonbr@gmail.com>
2020-01-26 11:38:26 +0200

    user and profile remove

--- config/db.js
+++ config/db.js
@@ -7,7 +7,8 @@ const connectDB = async () => {
     await mongoose.connect(db, {
       useCreateIndex: true,
       useUnifiedTopology: true,
-      useNewUrlParser: true
+      useNewUrlParser: true,
+      useFindAndModify: false
     });
 
     console.log('MongoDB connected...');
